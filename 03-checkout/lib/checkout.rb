287c0f4ffcee3022ba44c69d8add9bc5126e6ad8
Paul Martin <communty.hivemind@gmail.com>
2019-05-03 08:49:17 +0100

    Refactor

--- 03-checkout/lib/checkout.rb
+++ 03-checkout/lib/checkout.rb
@@ -1,8 +1,5 @@
 def checkout(basket)
   price = 50 * basket.length
-  if basket.length > 2
-    discount = (basket.length/3) * 20
-    price -= discount
-  end
-  price
+  discount = (basket.length/3) * 20
+  price - discount
 end
