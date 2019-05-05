c9b8c4e4d579fca6bb754670485f047728bd5c88
Paul Martin <communty.hivemind@gmail.com>
2019-05-05 08:20:42 +0100

    REFACTOR: remove redundant test

--- 04-checkout/spec/shop_spec.rb
+++ 04-checkout/spec/shop_spec.rb
@@ -1,7 +1,6 @@
 require 'shop'
 
 describe Shop do
-  it { is_expected.to respond_to(:checkout).with(1).argument }
   it 'returns 50 when passed "A"' do
     shop = Shop.new
     expect(shop.checkout('A')).to eq 50
