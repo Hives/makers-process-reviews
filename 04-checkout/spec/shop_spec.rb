0dd72bacd1c92c9836ab286d880cb652b7f6b5c0
Paul Martin <communty.hivemind@gmail.com>
2019-05-05 08:22:53 +0100

    Refactor tests to use explicit subject

--- 04-checkout/spec/shop_spec.rb
+++ 04-checkout/spec/shop_spec.rb
@@ -1,12 +1,13 @@
 require 'shop'
 
 describe Shop do
+  subject(:shop) { described_class.new }
+
   it 'returns 50 when passed "A"' do
-    shop = Shop.new
     expect(shop.checkout('A')).to eq 50
   end
+
   it 'returns 100 when passed "AA"' do
-    shop = Shop.new
     expect(shop.checkout('AA')).to eq 100
   end
 end
