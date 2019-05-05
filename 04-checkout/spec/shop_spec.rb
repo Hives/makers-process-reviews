1c0a59db06991d095e2b160b1af5549a56383647
Paul Martin <communty.hivemind@gmail.com>
2019-05-05 08:28:59 +0100

    REFACTOR: wrap "A" tests in a context block

--- 04-checkout/spec/shop_spec.rb
+++ 04-checkout/spec/shop_spec.rb
@@ -3,23 +3,25 @@ require 'shop'
 describe Shop do
   subject(:shop) { described_class.new }
 
-  it 'returns 50 when passed "A"' do
-    expect(shop.checkout('A')).to eq 50
-  end
+  context 'Lots of "A"s' do
+    it 'returns 50 when passed "A"' do
+      expect(shop.checkout('A')).to eq 50
+    end
 
-  it 'returns 100 when passed "AA"' do
-    expect(shop.checkout('AA')).to eq 100
-  end
+    it 'returns 100 when passed "AA"' do
+      expect(shop.checkout('AA')).to eq 100
+    end
 
-  it 'returns 130 when passed "AAA"' do
-    expect(shop.checkout('AAA')).to eq 130
-  end
+    it 'returns 130 when passed "AAA"' do
+      expect(shop.checkout('AAA')).to eq 130
+    end
 
-  it 'returns 180 when passed "AAAA"' do
-    expect(shop.checkout('AAAA')).to eq 180
-  end
+    it 'returns 180 when passed "AAAA"' do
+      expect(shop.checkout('AAAA')).to eq 180
+    end
 
-  it 'returns 260 when passed "AAAAAA"' do
-    expect(shop.checkout('AAAAAA')).to eq 260
+    it 'returns 260 when passed "AAAAAA"' do
+      expect(shop.checkout('AAAAAA')).to eq 260
+    end
   end
 end
