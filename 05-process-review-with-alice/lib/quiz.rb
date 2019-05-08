c75723cadc378ea724ff3ee95035e08162ba818c
Paul Martin <communty.hivemind@gmail.com>
2019-05-08 17:46:17 +0100

    POST-REVIEW COMMIT only run quiz if script was main
    This:
    
    ```
    if __FILE__==$0
      q = Quiz.new
      q.run
    end
    ```
    
    Means the code inside will only run if it's the main script, not if it's
    load'd or require'd. That means it won't run in your RSpec tests! Got
    stuck on this for like 15 minutes...... LOL

--- 05-process-review-with-alice/lib/quiz.rb
+++ 05-process-review-with-alice/lib/quiz.rb
@@ -1,8 +1,16 @@
 class Quiz
+  def initialize(kernel = Kernel)
+    @kernel = kernel
+  end
+
   def run
     puts "What is the capital of France?"
+    # @kernel.gets
+    # puts "Correct!"
   end
 end
 
-q = Quiz.new
-q.run
+if __FILE__==$0
+  q = Quiz.new
+  q.run
+end
--- 05-process-review-with-alice/spec/quiz_spec.rb
+++ 05-process-review-with-alice/spec/quiz_spec.rb
@@ -4,7 +4,16 @@ describe Quiz do
   describe "#run" do
     it "Outputs a question to the screen" do
       quiz = Quiz.new
-      expect { quiz.run }.to output("What is the capital of France?\n").to_stdout
+      expect(STDOUT).to receive(:puts).with('What is the capital of France?')
+      quiz.run
+    end
+
+    xit "Accepts input from the user" do
+      kernel = double(:kernel)
+      allow(kernel).to receive(:gets).and_return("France\n")
+      quiz = Quiz.new(kernel)
+      quiz.run
+      expect { kernel.gets }.to output("Correct!").to_stdout
     end
   end
 end
